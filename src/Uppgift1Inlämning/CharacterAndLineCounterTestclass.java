0ba9f690c0ae4098a40a41a0041ae3685668578e
albin <albinsahiti47@gmail.com>
2024-11-14 11:51:38 +0100

    Updated testcases

--- src/Uppgift1Inlämning/CharacterAndLineCounterTestclass.java
+++ src/Uppgift1Inlämning/CharacterAndLineCounterTestclass.java
@@ -8,15 +8,47 @@ import org.junit.jupiter.api.Test;
 public class CharacterAndLineCounterTestclass {
 
     @Test
-    public void stopTestCase() {
+    public void linecountTestCase() {
+
+        Counter counter = new Counter();
+
+        counter.addLine("Hej");
+
+        counter.addLine("Jag heter Albin");
+
+        assertEquals(2,counter.getLineCount());
+
+
+    }
+
+
+    @Test
+    public void charcountTestCase() {
+
+        Counter counter = new Counter();
+
+        counter.addLine("Hej");
+
+        counter.addLine("Jag heter Albin");
+
+        assertEquals(18,counter.getCharCount());
 
 
 
     }
 
 
+    @Test
+    public void startconditionTestCase() {
+
+        Counter counter = new Counter();
+        ;
+
+        assertEquals(0,counter.getLineCount());
+
+        assertEquals(0,counter.getCharCount());
+
+    }
 
 
-
-
-}
+    }
